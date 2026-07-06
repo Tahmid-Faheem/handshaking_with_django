a5c6b3028c360103bac885931edf7571dcb024c5
Tahmid-Fahim <tahmid.cse.cu@gmail.com>
2026-07-07 03:41:52 +0600

    type_hints are indeed halpful to get the vaibe of c data types in dynamically typed language like python

---
+++ type_hints.py
@@ -0,0 +1,28 @@
+"""Python is dynamically typed. Still we need type hint. Those don't force the type though. """
+# Wroks, as cpython directly ignores type hint
+def fetch_age(name: str) -> int:
+    return "This is a string."
+
+print(fetch_age("Thamid"))
+
+# Not too modern way but fine
+from typing import Optional, Iterable
+
+def find_user(user_id: int) -> Optional[dict]:
+    db = {1: {"name": "Tahmid"}}
+    return db.get(user_id)
+
+print(find_user(1))
+
+# Dataclass as shortcut of large classes
+from dataclasses import dataclass
+
+@dataclass
+class GPSpoints:
+    lat:float
+    long: float
+    timestamp: float
+    speed: float = 0.0
+
+v = GPSpoints(1,23,65,45)
+print(v)
